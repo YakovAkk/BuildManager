5eef3e9d2327fa122ab73cede4594e3840c00a8f
YakovAkk <james.yasko.2002@gmail.com>
2022-01-01 21:54:25 +0200

    I have added closing application command

---
+++ BuildManager/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BuildManager.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string ProtertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(ProtertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field,T value, [CallerMemberName] string ProtertyName = null)
+        {
+            if (Equals(field, value))
+            { 
+                return false; 
+            }
+            else
+            {
+                field = value;
+                OnPropertyChanged(ProtertyName);
+                return true;
+            }
+        }
+    }
+}
---
+++ BuildManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using BuildManager.Commands;
+using BuildManager.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BuildManager.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        #region Title
+        private string _Title;
+        public string Title
+        {
+            get
+            { 
+                return _Title;
+            }
+
+            set
+            {
+                Set(ref _Title, value);
+            }
+        }
+        #endregion
+
+        #region Command
+
+        #region CloseAppCommand
+        public ICommand CloseAppCommand { get; }
+
+        private void OnCloseAppCommandExecuted (object o)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private bool CanCloseAppCommandExecute (object o)
+        {
+            return true;
+        }
+
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            CloseAppCommand = new LamdaCommand(OnCloseAppCommandExecuted, CanCloseAppCommandExecute);
+        }
+    }
+}
